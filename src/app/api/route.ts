fbeeb7a01b51218ea4bcf4f7fbe4c451365a0fa5
brunoqwer84 <brunoqwer84@gmail.com>
2024-03-11 19:53:28 -0300

    MASSEY

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -15,7 +15,7 @@ import { NextResponse} from 'next/server';
 export async function GET(request:Request) {
     try {
         const result = 
-            await sql `SELECT * FROM LICENCAS`;
+            await sql `SELECT * FROM LICENCAS WHERE ID_CLIENTE = '2'`;
 
             return NextResponse.json({result}, {status: 200});
     } catch (error){

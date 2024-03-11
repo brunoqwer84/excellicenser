a497015f65e761b7c7fa36a1b2d521639ca01420
brunoqwer84 <brunoqwer84@gmail.com>
2024-03-11 20:54:11 -0300

    eurekaa

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -9,15 +9,18 @@ return NextResponse.json({
 });
 
 }*/
+import { unstable_noStore } from 'next/cache';
 import {sql} from '@vercel/postgres';
 import { NextResponse} from 'next/server';
 
 export async function GET(request:Request) {
     try {
+        unstable_noStore()
         const result = 
-            await sql `SELECT * FROM LICENCAS WHERE ID_CLIENTE = '2'`;
+            await sql `SELECT * FROM LICENCAS`
+            return NextResponse.json({ result }, { status: 200, headers: { 'Cache-Control': 'no-store, max-age=0' } });
 
-            return NextResponse.json({result}, {status: 200});
+           /* return NextResponse.json({result}, {status: 200});*/
     } catch (error){
       return NextResponse.json({error}, {status: 500});
     }
